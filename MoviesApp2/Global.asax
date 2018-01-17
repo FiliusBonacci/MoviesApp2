e73ee5f8b22d2e6e0d6f2be3b239475d39ce1468
Paweł Jadanowski <pjadanowski@sigma.ug.edu.pl>
2018-01-17 14:14:32 +0100

    Dodaj pliki projektów.

---
+++ MoviesApp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviesApp2.MvcApplication" Language="C#" %>
---
+++ MoviesApp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesApp2.Startup))]
+namespace MoviesApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
